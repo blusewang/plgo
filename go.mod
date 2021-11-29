7f830d7fb5ed5a84170f88ccf1dc2122b59f4596
blusewang <dev.jf.wang@gmail.com>
2021-11-29 16:50:38 +0800

    建立

---
+++ go.mod
@@ -0,0 +1,7 @@
+module gitea.mywsy.cn/jfwang/plgo
+
+go 1.17
+
+require (
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881
+)
---
+++ plgo/pathnames.go
@@ -0,0 +1,24 @@
+//go:build !windows
+// +build !windows
+
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+// getcorrectpath used on Windows, see file pathnames_windows.go
+func getcorrectpath(p string) string {
+	ret := strings.TrimRight(p, "\n")
+	return ret
+}
+
+// addOtherIncludesAndLDFLAGS used on Windows, see file pathnames_windows.go
+func addOtherIncludesAndLDFLAGS(plgoSource *string, postgresIncludeDir string) {
+	return
+}
+
+func buildPath() (string, error) {
+	return ioutil.TempDir("", plgo)
+}
